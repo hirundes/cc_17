4d833f2ab6e857ddbe949a649a399c129ef1e569
HIRUN <hirundesilva97@gmail.com>
2025-03-25 13:55:53 -0400

    Task 2 - Created SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -20,5 +20,26 @@ firstCustomer.addPurchase(100)
 console.log(`${firstCustomer.name} total Spent: $${firstCustomer.getTotalSpent()}`);
 
 
+//Task 2
+class SalesRep {
+    constructor(name, clients) {
+        this.name = name;
+        this.clients = [];
+    }
+    addClient(customer) {
+        this.clients.push(customer);
+    }
+
+    getClientTotal(name) {
+        let client = this.client.find(client => client.name === name);
+        if (client) {
+            return client.getTotalSpent();
+        } else {
+            return 0;
+        }
+
+        }
+        
+    }
 
 
