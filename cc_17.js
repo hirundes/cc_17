df281bdf7350f8780c127c9fdb1956ef80785226
HIRUN <hirundesilva97@gmail.com>
2025-03-25 13:55:11 -0400

    Task 1 - Created Customer Class

--- cc_17.js
+++ cc_17.js
@@ -1,1 +1,24 @@
-console.log("hi");
+//Task 1
+class Customer {
+    constructor(name, email, purchaseHistory) {
+        this.name = name;
+        this.email = email;
+        this.purchaseHistory = [];
+    }
+    addPurchase(amount) {
+        this.purchaseHistory.push(amount);
+    }
+
+    getTotalSpent() {
+        return this.purchaseHistory.reduce((total, amount) => total + amount, 0)
+    }
+
+}
+
+let firstCustomer = new Customer(`Customer Name: ${"Hirun"}, Email: ${"hirundesilva@gmail.com"}`)
+firstCustomer.addPurchase(100)
+console.log(`${firstCustomer.name} total Spent: $${firstCustomer.getTotalSpent()}`);
+
+
+
+
