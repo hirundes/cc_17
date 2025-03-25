5d0acd6207788716ddf112685cf63e38bdd44f31
HIRUN <hirundesilva97@gmail.com>
2025-03-25 15:03:11 -0400

    Task 3 – Extended VIPCustomer class

--- cc_17.js
+++ cc_17.js
@@ -1,23 +1,36 @@
 //Task 1
 class Customer {
-    constructor(name, email, purchaseHistory) {
+    constructor(name, email, purchaseHistory, vipLevel) {
         this.name = name;
         this.email = email;
         this.purchaseHistory = [];
+        this.vipLevel = vipLevel;
     }
     addPurchase(amount) {
         this.purchaseHistory.push(amount);
     }
 
-    getTotalSpent() {
-        return this.purchaseHistory.reduce((total, amount) => total + amount, 0)
+    getTotalSpent() { //Task 3 Override with vipLevel
+        let total = this.purchaseHistory.reduce((total, amount) => total + amount, 0) //Task 3 Override 
+        if (this.vipLevel) {
+            return total * 1.1
+        } else {
+            return total;
+        }
+        }
     }
 
-}
 
-let firstCustomer = new Customer(`Customer Name: ${"Hirun"}, Email: ${"hirundesilva@gmail.com"}`)
-firstCustomer.addPurchase(100)
-console.log(`${firstCustomer.name} total Spent: $${firstCustomer.getTotalSpent()}`);
+console.log("----Task 1----")
+let firstCustomer = new Customer("Hirun Desilva", "hirundesilva97@gmail.com")
+firstCustomer.addPurchase(100);
+console.log(`Customer Name: ${firstCustomer.name}`);
+console.log(`Total Spent: $${firstCustomer.getTotalSpent()}`);
+
+let secondCustomer = new Customer("Jack Desilva", "jackdesilva@gmail.com");
+secondCustomer.addPurchase(200);
+console.log(`Customer Name: ${secondCustomer.name}`);
+console.log(`Total Spent: $${secondCustomer.getTotalSpent()}`);
 
 
 //Task 2
@@ -42,4 +55,32 @@ class SalesRep {
         
     }
 
+console.log("----Task 2----")
+let salesRep = new SalesRep("Michael Scott");
+salesRep.addClient(firstCustomer);
+salesRep.addClient(secondCustomer);
+console.log(`Sales Rep: ${salesRep.name}`);
+salesRep.clients.forEach(client => {
+    console.log(`Client: ${client.name}, Total Spent: $${client.getTotalSpent()}`);
+}
+);
+
+//Task 3 
+class VIPCustomer extends Customer {
+    constructor(name, email, purchaseHistory, vipLevel) {
+        super(name, email, purchaseHistory);
+        this.vipLevel = vipLevel
+    }
+  
+    getTotalSpent() {
+        let total = super.getTotalSpent();
+        return total * 1.1;
+    }
+};
+
+console.log("----Task 3----")
+let vipCustomers = new VIPCustomer("Will Smith, willsmith@gmail.com, Platinum")
+vipCustomers.addPurchase(150)
+console.log(`VIP Customer Information: ${vipCustomers.name}`);
+console.log(`Total Spent with 10% Loyalty Bonus: ${vipCustomers.getTotalSpent()}`)
 
